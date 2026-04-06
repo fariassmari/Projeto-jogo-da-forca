66cea444cf6aac10353c8ec4045830501b2f69ad
fariassmari <marianafariasbrito@gmail.com>
2026-04-06 20:39:50 -0300

    últimas alterações do projeto

--- src/Teste.java
+++ src/Teste.java
@@ -1,7 +1,58 @@
 import javax.swing.*;
+import java.awt.Component;
+import java.util.ArrayList;
 
 public class Teste {
-    private JPanel panel1;
-    private JButton palpiteButton;
-    private JTextField digiteUmaLetraAquiTextField;
+    private String letraDigitada;
+    private ArrayList<Integer> ocorrencias;
+    private JogoDaForca jogo = new JogoDaForca();
+
+    public Teste() {
+        JOptionPane.showMessageDialog(null,
+                "Bem-vindo ao Jogo da Forca!\nTente adivinhar a palavra.");
+
+        jogo.iniciar();
+
+        do {
+            letraDigitada = JOptionPane.showInputDialog(null,
+                    "Dica: " + jogo.getDica() +
+                            "\nPalavra: " + jogo.getPalavra() +
+                            "\nDigite uma letra:");
+
+            try {
+                if (letraDigitada == null || letraDigitada.isEmpty()) {
+                    throw new Exception("Digite uma letra válida!");
+                }
+
+                ocorrencias = jogo.getOcorrencias(letraDigitada);
+
+                if (ocorrencias.size() > 0) {
+                    JOptionPane.showMessageDialog(null,
+                            "Você acertou a letra: " + letraDigitada +
+                                    "\n\nPalavra: " + jogo.getPalavra() +
+                                    "\nAcertos: " + jogo.getAcertos() +
+                                    "\nPenalidade: " + jogo.getCodigoPenalidade() +
+                                    " - " + jogo.getNomePenalidade());
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Você errou a letra: " + letraDigitada +
+                                    "\n\nPalavra: " + jogo.getPalavra() +
+                                    "\nAcertos: " + jogo.getAcertos() +
+                                    "\nPenalidade: " + jogo.getCodigoPenalidade() +
+                                    " - " + jogo.getNomePenalidade());
+                }
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+
+        } while (!jogo.terminou());
+
+        JOptionPane.showMessageDialog(null,
+                "Resultado final: " + jogo.getResultado());
+    }
+
+    public static void main(String[] args) {
+        new Teste();
+    }
 }
---
+++ src/Teste2.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Teste2 {
+    private JPanel panel1;
+    private JButton palpiteButton;
+    private JTextField digiteUmaLetraAquiTextField;
+}
